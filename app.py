82b49afdb15493cf205f0377883cd5ffba5b4b43
Anna Kivekäs <anna.kivekas@icloud.com>
2025-09-19 13:56:01 +0300

    Lisää toimintoja sovellukseen

--- app.py
+++ app.py
@@ -1,38 +1,23 @@
-from flask import Flask, render_template, redirect, url_for, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
+from flask import Flask, render_template, redirect, url_for, request, abort
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from forms import RegisterForm, LoginForm, WorkoutForm
+from forms import RegisterForm, LoginForm, WorkoutForm, MessageForm
+from models import db, User, Workout, Message
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///workout_diary.db'
-db = SQLAlchemy(app)
+
+db.init_app(app)
 
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
 
-# --- MODELS ---
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password_hash = db.Column(db.String(150), nullable=False)
-    workouts = db.relationship('Workout', backref='user', lazy=True)
-
-class Workout(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.String(50))
-    type = db.Column(db.String(50))
-    duration = db.Column(db.Integer)
-    description = db.Column(db.Text)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# --- ROUTES ---
 @app.route('/')
 @login_required
 def index():
@@ -82,6 +67,67 @@ def add_workout():
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('workout_form.html', form=form)
+
+@app.route('/messages', methods=['GET', 'POST'])
+@login_required
+def messages():
+    form = MessageForm()
+    if form.validate_on_submit():
+        new_message = Message(content=form.content.data, user_id=current_user.id)
+        db.session.add(new_message)
+        db.session.commit()
+        return redirect(url_for('messages'))
+
+    all_messages = Message.query.all()
+    return render_template('messages.html', form=form, messages=all_messages)
+
+@app.route('/edit_message/<int:message_id>', methods=['GET', 'POST'])
+@login_required
+def edit_message(message_id):
+    message = Message.query.get_or_404(message_id)
+    if message.user_id != current_user.id:
+        abort(403)
+    form = MessageForm()
+    if form.validate_on_submit():
+        message.content = form.content.data
+        db.session.commit()
+        return redirect(url_for('messages'))
+
+    form.content.data = message.content
+    return render_template('edit_message.html', form=form, message=message)
+
+
+@app.route('/delete_message/<int:message_id>')
+@login_required
+def delete_message(message_id):
+    message = Message.query.get_or_404(message_id)
+    if message.user_id != current_user.id:
+        abort(403)
+
+    db.session.delete(message)
+    db.session.commit()
+    return redirect(url_for('messages'))
+
+@app.route('/search')
+@login_required
+def search():
+    query = request.args.get('query')
+    if query:
+        results = Workout.query.filter(
+            (Workout.type.ilike(f"%{query}%")) |
+            (Workout.description.ilike(f"%{query}%")) |
+            (Workout.date.ilike(f"%{query}%"))
+        ).filter_by(user_id=current_user.id).all()
+    else:
+        results = []
+    return render_template('search.html', query=query, results=results)
+
+@app.route('/user/<int:user_id>')
+@login_required
+def show_user(user_id):
+    user = User.query.get_or_404(user_id)
+    workouts = Workout.query.filter_by(user_id=user_id).order_by(Workout.date.desc()).all()
+    return render_template('user.html', user=user, workouts=workouts)
 
 if __name__ == '__main__':
     with app.app_context():
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, IntegerField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, TextAreaField, SelectField
 from wtforms.validators import InputRequired, Length
 
 class RegisterForm(FlaskForm):
@@ -14,7 +14,18 @@ class LoginForm(FlaskForm):
 
 class WorkoutForm(FlaskForm):
     date = StringField('Päivämäärä', validators=[InputRequired()])
-    type = StringField('Treenityyppi', validators=[InputRequired()])
+    type = SelectField('Treenityyppi', choices=[
+        ('juoksu', 'Juoksu'),
+        ('pyöräily', 'Pyöräily'),
+        ('uinti', 'Uinti'),
+        ('kävely', 'Kävely'),
+        ('kuntosali', 'Kuntosali'),
+        ('jooga', 'Jooga')
+    ], validators=[InputRequired()])
     duration = IntegerField('Kesto (min)', validators=[InputRequired()])
     description = TextAreaField('Kuvaus')
     submit = SubmitField('Lisää treeni')
+
+class MessageForm(FlaskForm):
+    content = TextAreaField('Viestin sisältö', validators=[InputRequired()])
+    submit = SubmitField('Lähetä viesti')
