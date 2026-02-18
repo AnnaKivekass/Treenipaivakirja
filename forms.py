e700bb7dbfd19f695096a3fd1c23759bc1418ef8
Anna Kivekäs <anna.kivekas@helsinki.fi>
2026-02-18 11:59:55 +0200

    Remove WTFforms and clean duplicate HTML files

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
-
-
-class EditMessageForm(FlaskForm):
-    title = StringField("Otsikko", validators=[DataRequired(), Length(max=200)])
-    content = TextAreaField("Sisältö", validators=[DataRequired(), Length(max=5000)])
-    submit = SubmitField("Tallenna")
